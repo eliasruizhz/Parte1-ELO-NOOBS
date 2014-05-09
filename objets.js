72c0ce89af33bb5ca104204e1f4f4db6961bf6eb
Elias Ruiz Hz <conk_soldiers@hotmail.com>
2014-05-09 09:30:40 -0500

    Create objets.js

---
+++ objets.js
@@ -0,0 +1,22 @@
+var db = [
+{
+	"nombre": "cinco",
+	"puntaje": 0
+},
+{
+	"nombre": "cuatro",
+	"puntaje": 0
+},
+{
+	"nombre": "tres",
+	"puntaje": 0
+},
+{
+	"nombre": "dos",
+	"puntaje": 0
+},
+{
+	"nombre": "uno",
+	"puntaje": 0
+}
+];
